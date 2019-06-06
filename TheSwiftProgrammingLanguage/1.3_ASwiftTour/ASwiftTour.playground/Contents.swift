37c617f77803e712ebb74a87d62e6194afc0d263
Josh Cook <joshuacook0023@gmail.com>
2019-06-06 11:09:01 -0400

    Day 2/100 of 100 Days of Swift
    - introduction to objects and classes
    - touched upon getters and setters for classes
    - overview of enums and structs
    
    Coming from data science, these are OOP features that I have
    little experience with.

--- TheSwiftProgrammingLanguage/1.3_ASwiftTour/ASwiftTour.playground/Contents.swift
+++ TheSwiftProgrammingLanguage/1.3_ASwiftTour/ASwiftTour.playground/Contents.swift
@@ -267,3 +267,249 @@ numbers.sorted { $0 > $1 }
 
 
 // ---- Objects and Classes ---- //
+// variabls, constants, methods, functions look identical
+class Shape {
+    var numberOfSides = 0
+    let name = "Bob"
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+    func calcPerimeter(_ length: Double) -> Double {
+        return Double(numberOfSides) * length
+    }
+}
+
+// create an instance of the class
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+
+// initializers -- every property needs an initial value
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+// use `deinit` to deinitialize (cleanup before deallocation) (?)
+
+// subclasses
+// explicitly mark overridden methods with `override`
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)"
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+class Circle: NamedShape {
+    var radiusLength: Double
+    
+    init(radius: Double, name: String) {
+        self.radiusLength = radius
+        super.init(name: name)
+        numberOfSides = 1
+    }
+    
+    func area() -> Double {
+        return 3.14 * radiusLength * radiusLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A circle with a radius of length \(radiusLength)"
+    }
+}
+
+// property getters and setters
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init (sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set(newValue) {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
+// `willSet` and `didSet` to update values
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+
+// optional is classes
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
+
+
+// ---- Enumerations and Structures ---- //
+
+// enumerations
+// automatically starts at zero and continues in order
+// here, force it to start at 1 and it continues from there
+// can also specifically declare the values
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+// expt: a fxn to compare two Rank values by comparing their raw values
+func compareRanks(_ rank1: Rank, _ rank2: Rank) -> String {
+    if (rank1.rawValue > rank2.rawValue) {
+        return "Rank 1 is larger than Rank 2"
+    } else if (rank1.rawValue < rank2.rawValue) {
+        return "Rank 2 is larger than Rank 1"
+    } else {
+        return "Both Ranks are equal in value"
+    }
+}
+compareRanks(Rank.ace, Rank.jack)
+compareRanks(Rank.three, Rank.two)
+compareRanks(Rank.seven, Rank.seven)
+
+//
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+}
+
+// a case where the values of a enum do not matter
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+    
+    func color() -> String {
+        switch self {
+        case .spades, .clubs:
+            return "black"
+        case .hearts, .diamonds:
+            return "red"
+        }
+    }
+}
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+
+
+// if an enumeration has raw values, those are always the same for every instance
+// alternatively, the values can be created upon instantiation
+// these behave more like stored properties
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+}
+
+let success = ServerResponse.result("6:00AM", "8:09PM")
+let failure = ServerResponse.failure("Out of cheese.")
+
+switch success {
+case let .result(sunrise, sunset):
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+case let .failure(message):
+    print("Failure... \(message)")
+}
+
+// structures
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())."
+    }
+}
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+
+// ---- Protocols and Extensions ---- //
+

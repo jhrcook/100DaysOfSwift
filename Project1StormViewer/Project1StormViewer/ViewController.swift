79a74bb06de0743c3b813e4eb61af2a2a5ba367c
Josh Cook <joshuacook0023@gmail.com>
2019-06-27 16:35:44 -0700

    23 of 100 Days of Swift (cont.)
    I also took a few minutes at the end of day to start the "Storm
    Viewer" app project, the first example in Hacking with Swift.

---
+++ Project1StormViewer/Project1StormViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1StormViewer
+//
+//  Created by Joshua on 6/27/19.
+//  Copyright © 2019 JHC Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

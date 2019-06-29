3d90f308536f9e449b24ed6b5c10336879047be4
Josh Cook <joshuacook0023@gmail.com>
2019-06-28 17:06:07 -0700

    24 of 100 Days of Swift (cont.)
    At the end of the day, I decided to begin the next Project:
    creating the game "Guess the Flag." I learned about an app
    bundle's Assets.xcassets directory.

--- Project2GuessTheFlag/Project2GuessTheFlag/ViewController.swift
+++ Project2GuessTheFlag/Project2GuessTheFlag/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

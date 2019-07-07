eefab7e5e34479b728eadcae9f25c46836c164b3
Josh Cook <joshuacook0023@gmail.com>
2019-07-06 21:40:11 -0700

    32 of 100 Days of Swift
    I began the pachinko game app. I have set up the app and learned
    how to add `SKSpriteNode` objects and have them act according to
    accurate physics. I also used an `SKAction` to have the glows at
    the bottom rotate constantly.

---
+++ Project11Pachinko/Project11Pachinko/GameScene.swift
@@ -0,0 +1,72 @@
+//
+//  GameScene.swift
+//  Project11Pachinko
+//
+//  Created by Joshua on 7/6/19.
+//  Copyright © 2019 JHC Dev. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        for x in [0, 256, 512, 768, 1024] {
+            makeBouncer(at: CGPoint(x: x, y: 0))
+        }
+        var isGoodSwitch = true
+        for x in [128, 384, 640, 896] {
+            makeSlot(at: CGPoint(x: x, y: 0), isGood: isGoodSwitch)
+            isGoodSwitch = !isGoodSwitch
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody?.restitution = 0.4
+            ball.position = location
+            addChild(ball)
+        }
+    }
+    
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+    
+    func makeSlot(at postion: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+        }
+        
+        slotBase.position = postion
+        slotGlow.position = postion
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
+    }
+}
---
+++ Project11Pachinko/README.md
@@ -0,0 +1,24 @@
+# 100 Days of Swift - "Pachinko" iOS App
+
+![100DaysOfCodeSwift](https://img.shields.io/badge/100DaysOfCode-Swift-FA7343.svg?style=flat&logo=swift)
+[![jhc github](https://img.shields.io/badge/GitHub-jhrcook-lightgrey.svg?style=flat&logo=github)](https://github.com/jhrcook)
+[![jhc twitter](https://img.shields.io/badge/Twitter-JoshDoesaThing-00aced.svg?style=flat&logo=twitter)](https://twitter.com/JoshDoesa)
+[![jhc website](https://img.shields.io/badge/Website-JoshDoesaThing-5087B2.svg?style=flat&logo=telegram)](https://www.joshdoesathing.com)
+
+**Start Date: June 5, 2019  
+End Date: September 13, 2019**
+
+I want to learn how to program in the Swift language. To this end, I will practice coding in Swift for at least one hour every day for 100 days.
+
+This is an example iOS project produced by [*Hacking with Swift*](https://www.hackingwithswift.com/read) called ["Pachinko"](https://www.hackingwithswift.com/read/11/overview). This app is the game of pachinko. I will post images below of the view of the app after each day's work.
+
+## Daily progress of "Pachinko" app
+
+**Day 1 - July 6, 2019**
+
+I have set up the app and learned how to add `SKSpriteNode` objects and have them act according to accurate physics. I also used an `SKAction` to have the glows at the bottom rotate constantly.
+
+<img src="progress_screenshots/ezgif.com-video-to-gif.gi" width="500"/>
+
+
+
